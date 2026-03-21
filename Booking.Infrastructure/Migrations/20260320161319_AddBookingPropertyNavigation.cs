8b3fbd8fe6735bb6c35d3873d4333a22218aab5c
Lorena Malaj <lorena.malaj2fshnstudent.info>
2026-03-21 11:44:25 +0100

    feat: Add navigation properties

---
+++ Booking.Infrastructure/Migrations/20260320161319_AddBookingPropertyNavigation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingPropertyNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PropertyId1",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_PropertyId1",
+                table: "Bookings",
+                column: "PropertyId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Properties_PropertyId1",
+                table: "Bookings",
+                column: "PropertyId1",
+                principalTable: "Properties",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Properties_PropertyId1",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_PropertyId1",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PropertyId1",
+                table: "Bookings");
+        }
+    }
+}
---
+++ Booking.Infrastructure/Migrations/20260321094930_AddPropertyAddressNavigation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyAddressNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Properties_PropertyId1",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_PropertyId1",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PropertyId1",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId1",
+                table: "Properties",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_AddressId1",
+                table: "Properties",
+                column: "AddressId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Properties_Addresses_AddressId1",
+                table: "Properties",
+                column: "AddressId1",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Properties_Addresses_AddressId1",
+                table: "Properties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Properties_AddressId1",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId1",
+                table: "Properties");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PropertyId1",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_PropertyId1",
+                table: "Bookings",
+                column: "PropertyId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Properties_PropertyId1",
+                table: "Bookings",
+                column: "PropertyId1",
+                principalTable: "Properties",
+                principalColumn: "Id");
+        }
+    }
+}
