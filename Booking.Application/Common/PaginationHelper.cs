e8f756e9c20b21f721bb575b6f853b82884252b9
Lorena Malaj <lorena.malaj2fshnstudent.info>
2026-03-21 11:45:15 +0100

    feat: Implement pagination

---
+++ Booking.Application/Common/PaginationHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Booking.Application.Common;
+
+public class PagedResult<T>
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public List<T> Data { get; set; } = new();
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+
+    public PagedResult(List<T> data, int count, int page, int pageSize)
+    {
+        Data = data;
+        TotalCount = count;
+        Page = page;
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        HasNextPage = Page < TotalPages;
+        HasPreviousPage = Page > 1;
+    }
+}
